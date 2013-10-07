73b82bf1e7ec982b6b7770f82fd72d08659fd43c
ekate Kuznetsova <kuznetsova@localhost>
2013-10-06 19:41:52 -0700

    Please enter the commit message for your changes. Lines starting

--- game.js
+++ game.js
@@ -3,10 +3,15 @@ $(document).ready(function(){
         var ctx = canvas.getContext("2d");
 
         // Fun;
-        var fun = 0
+        var fun = 0;
+        var coordX = -1;
+        var coordY = -1; 
 
 	function start_game()
 	{
+                var img = document.getElementById("canvas1");
+                ctx.drawImage(img, 0, 0);
+
 		// Main move loop.
 		if(typeof game_loop != "undefined") clearInterval(game_loop);
 		game_loop = setInterval(score, 100);
@@ -15,17 +20,32 @@ $(document).ready(function(){
 	start_game();
 	function score()
 	{
+
+                var offset = 100;               
+
                 // Clear screen.
                 ctx.fillStyle = "white";
-	        ctx.fillRect(0,0, 100, 100); 
-   
+	        ctx.fillRect(0,0+offset, 115, 30+offset);
+                
                 // Count.               
                 fun++;
           
                 // Display.
                 var fun_text = "FUN: " + fun;
                 ctx.fillStyle = "blue";
-	        ctx.fillText(fun_text, 50, 50);
+	        ctx.fillText(fun_text, 50, 50+offset);
+                fun_text = "ClickX: " + coordX;
+	        ctx.fillText(fun_text, 50, 50+offset+10);
+                fun_text = "ClickY: " + coordY;
+	        ctx.fillText(fun_text, 50, 50+offset+20);
+
 	}
+
+	// Keyboard controls
+	$(document).click(function(e){
+              coordX = e.clientX;
+              coordY = e.clientY;
+              fun = 0;
+	})
 
 })
